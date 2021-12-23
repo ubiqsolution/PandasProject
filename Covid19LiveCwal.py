9eaa186fcde897a8c8541a1961acaa122139d1e0
ubiqsolution <9292@ubiqsolution.com>
2021-12-23 21:05:18 +0900

    하단 함수 실행부 소스 수정!

--- Covid19LiveCwal.py
+++ Covid19LiveCwal.py
@@ -57,5 +57,6 @@ def localcovid():
     print('주간 일평균 코로나 확진자 수 : ' + covid_confirmed_reverge_txt + '명') # 주간 일평균 코로나 확진자수 출력
     print('')
 
-worldcovid()
-localcovid()
+if __name__ == "__main__":
+    worldcovid()
+    localcovid()
